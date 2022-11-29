7c574650b78c698b0f4eb05c268b4d07c723b55a
minhnhatle104 <lnminh104@gmail.com>
2022-11-29 10:55:38 +0700

    09_datVeReaiTime

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/configStore';
-import 'antd/dist/antd.css';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//Cấu hình realtime (websocket với signalR)
+import * as signalR from '@aspnet/signalr'
+
+//antd
+import "antd/dist/antd.css";
+
+//reactslick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { DOMAIN } from './util/settings/config';
+
+
+//Đoạn code để kết nối đến server lắng nghe sự kiện từ server
+export const connection = new signalR.HubConnectionBuilder().withUrl(`${DOMAIN}/DatVeHub`).configureLogging(signalR.LogLevel.Information).build();
+
+connection.start().then(() => {
+  ReactDOM.render(
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
-);
+    ,
+    document.getElementById('root')
+  );
+}).catch(errors => {
+  console.log(errors);
+})
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/reducers/QuanLyDatVeReducer.js
+++ src/redux/reducers/QuanLyDatVeReducer.js
@@ -4,11 +4,14 @@ import {ThongTinLichChieu} from '../../_core/models/ThongTinPhongVe'
 
 const stateDefault = {
     chiTietPhongVe: new ThongTinLichChieu(),
-    danhSachGheDangDat: [],
-    tabActive : "1"
+    danhSachGheDangDat: [], //danh sách ghế đang đặt
+    danhSachGheKhachDat: [],
+    // [{maGhe:48041},{maGhe:48042}],
+    tabActive: '1'
 }
 
 export const QuanLyDatVeReducer = (state=stateDefault,action)=> {
+
     switch (action.type) {
        
         case SET_CHI_TIET_PHONG_VE: {
@@ -31,18 +34,25 @@ export const QuanLyDatVeReducer = (state=stateDefault,action)=> {
             return {...state,danhSachGheDangDat:danhSachGheCapNhat}
         }
 
-        case DAT_VE_HOAN_TAT:{
-            state.danhSachGheDangDat = []
+
+        case DAT_VE_HOAN_TAT :{
+            state.danhSachGheDangDat = [];
             return {...state}
         }
 
-        case CHUYEN_TAB:{
-            state.tabActive = "2"
-            return {...state}
+        case CHUYEN_TAB : {
+            state.tabActive = '2';
+            return {...state};
         }
 
-        case "CHANGE_TAB_ACTIVE":{
-            state.tabActive = action.number
+        case 'CHANGE_TAB_ACTIVE' : {
+            console.log('action',action)
+            state.tabActive = action.number;
+            return {...state};
+        }
+
+        case 'DAT_GHE' : {
+            state.danhSachGheKhachDat = action.arrGheKhachDat;
             return {...state}
         }
 
